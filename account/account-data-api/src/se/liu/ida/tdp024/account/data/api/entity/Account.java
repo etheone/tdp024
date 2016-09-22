e41097cf572b3d04c0b468315345e80052a45899
Emil Nilsson <emini901@li1-4.ida.liu.se>
2016-09-22 15:05:22 +0200

    Added some updates

--- account/account-data-api/src/se/liu/ida/tdp024/account/data/api/entity/Account.java
+++ account/account-data-api/src/se/liu/ida/tdp024/account/data/api/entity/Account.java
@@ -4,4 +4,23 @@ import java.io.Serializable;
 
 public interface Account extends Serializable {
     
+    public long getId();
+    
+    public void setId(long id);
+    
+    public String getPersonKey();
+    
+    public void setPersonKey(String key);
+    
+    public String getAccountType();
+    
+    public void setAccountType(String type);
+    
+    public String getBankKey();
+    
+    public void setBankKey(String key);
+    
+    public long getHoldings();
+    
+    public void setHoldings(long amount);
 }
