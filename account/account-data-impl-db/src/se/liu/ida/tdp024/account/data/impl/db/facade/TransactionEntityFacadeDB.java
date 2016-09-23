2608c4c5a0ce06e6b66b51afc2ee1f215d80c07c
Tommy Lindman <tomli962@student.liu.se>
2016-09-23 15:09:08 +0200

    Started creating tests for the class AccountEntityFacadeDB.

--- account/account-data-impl-db/src/se/liu/ida/tdp024/account/data/impl/db/facade/TransactionEntityFacadeDB.java
+++ account/account-data-impl-db/src/se/liu/ida/tdp024/account/data/impl/db/facade/TransactionEntityFacadeDB.java
@@ -5,10 +5,82 @@
  */
 package se.liu.ida.tdp024.account.data.impl.db.facade;
 
+import java.util.List;
+import java.util.ServiceConfigurationError;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import se.liu.ida.tdp024.account.data.api.entity.Account;
+import se.liu.ida.tdp024.account.data.api.entity.Transaction;
+import se.liu.ida.tdp024.account.data.api.facade.TransactionEntityFacade;
+import se.liu.ida.tdp024.account.data.impl.db.entity.TransactionDB;
+import se.liu.ida.tdp024.account.data.impl.db.util.EMF;
+
 /**
  *
  * @author emini901
  */
-public class TransactionEntityFacadeDB {
+public class TransactionEntityFacadeDB implements TransactionEntityFacade{
+
+    @Override
+    public long create(String type, long amount, 
+            String date, String status, Account account) {
+        
+        EntityManager em = EMF.getEntityManager();
+        
+        try {
+            
+            em.getTransaction().begin();
+            
+            Transaction transaction = new TransactionDB();
+            transaction.setType(type);
+            transaction.setAmount(amount);
+            transaction.setDate(date);
+            transaction.setStatus(status);
+            transaction.setAccount(account);
+            
+            em.persist(transaction);
+            em.getTransaction().commit();
+            
+            return transaction.getId();
+        } catch (Exception e) {
+            // Log stuff
+            throw new ServiceConfigurationError("Commit failed.");
+        } finally {
+            
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            em.close();
+        }
+    }
+
+    @Override
+    public Transaction find(long id) {
+        EntityManager em = EMF.getEntityManager();
+        
+        try {
+            return em.find(TransactionDB.class, id);
+        } catch(Exception e) {
+            //logg stuff
+            return null;
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public List<Transaction> findAll(Account account) {
+        EntityManager em = EMF.getEntityManager();
+        
+        try {
+            Query query = em.createQuery("SELECT t from TransactionDB t WHERE t.account.id = " + account.getId());
+            return query.getResultList();
+        } catch(Exception e) {
+            //log
+            return null;
+        } finally {
+            em.close();
+        }
+    }
     
 }
