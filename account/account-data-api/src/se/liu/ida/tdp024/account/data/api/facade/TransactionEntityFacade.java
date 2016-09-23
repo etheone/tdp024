4236bd5c6808f748f74bf9e0fe20b1db3c9b923c
Tommy Lindman <tomli962@student.liu.se>
2016-09-23 13:29:55 +0200

    Started implementing the transaction facade.

--- account/account-data-api/src/se/liu/ida/tdp024/account/data/api/facade/TransactionEntityFacade.java
+++ account/account-data-api/src/se/liu/ida/tdp024/account/data/api/facade/TransactionEntityFacade.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package se.liu.ida.tdp024.account.data.api.facade;
+
+import java.util.List;
+import se.liu.ida.tdp024.account.data.api.entity.Account;
+import se.liu.ida.tdp024.account.data.api.entity.Transaction;
 
 /**
  *
- * @author emini901
+ * @author emini901, tomli962
  */
 public interface TransactionEntityFacade {
+    public long create(String type, long amount,
+            String date, String status, Account account);
     
+    public Transaction find(long id);
+    
+    public List<Transaction> findAll(Account account);
 }
