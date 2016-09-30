3f7a99abf89be8709b9bc963590021dc3075b1eb
Tommy Lindman <tomli962@student.liu.se>
2016-09-30 15:12:32 +0200

    Work done in logic layer.

--- account/account-logic-impl/src/se/liu/ida/tdp024/account/logic/impl/facade/AccountLogicFacadeImpl.java
+++ account/account-logic-impl/src/se/liu/ida/tdp024/account/logic/impl/facade/AccountLogicFacadeImpl.java
@@ -1,7 +1,10 @@
 package se.liu.ida.tdp024.account.logic.impl.facade;
 
+import se.liu.ida.tdp024.account.data.api.entity.Account;
 import se.liu.ida.tdp024.account.data.api.facade.AccountEntityFacade;
 import se.liu.ida.tdp024.account.logic.api.facade.AccountLogicFacade;
+import se.liu.ida.tdp024.account.util.http.HTTPHelper;
+import se.liu.ida.tdp024.account.util.http.HTTPHelperImpl;
 
 public class AccountLogicFacadeImpl implements AccountLogicFacade {
     
@@ -9,6 +12,33 @@ public class AccountLogicFacadeImpl implements AccountLogicFacade {
     
     public AccountLogicFacadeImpl(AccountEntityFacade accountEntityFacade) {
         this.accountEntityFacade = accountEntityFacade;
+    }
+
+    @Override
+    public void create(String personName, String accountType, String bankName) {
+        // Contact our services to get personKey and bankKey
+        HTTPHelper httphelper = new HTTPHelperImpl();
+        String response = httphelper.get("http://localhost:8060/person/find.name","name", personName);
+        System.out.println();
+        System.out.println(response);
+        System.out.println(response.substring(response.indexOf("key")+4, response.indexOf("key")+10));
+        System.out.println();
+        if (!response.equals("null"))
+        {
+            String personKey = "PKey";
+            String bankKey = "BKey";    
+            accountEntityFacade.create(personKey, accountType, bankKey);
+        }   
+    }
+
+    @Override
+    public void addTransaction(long accoundId, long transactionId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Account findByName(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
--- account/account-logic-test/test/se/liu/ida/tdp024/account/logic/test/facade/AccountLogicFacadeTest.java
+++ account/account-logic-test/test/se/liu/ida/tdp024/account/logic/test/facade/AccountLogicFacadeTest.java
@@ -3,23 +3,25 @@ package se.liu.ida.tdp024.account.logic.test.facade;
 import org.junit.After;
 import org.junit.Test;
 import se.liu.ida.tdp024.account.data.api.util.StorageFacade;
+import se.liu.ida.tdp024.account.data.impl.db.facade.AccountEntityFacadeDB;
+import se.liu.ida.tdp024.account.data.impl.db.util.StorageFacadeDB;
 import se.liu.ida.tdp024.account.logic.api.facade.AccountLogicFacade;
+import se.liu.ida.tdp024.account.logic.impl.facade.AccountLogicFacadeImpl;
 
 public class AccountLogicFacadeTest {
 
     
     //--- Unit under test ---//
-    public AccountLogicFacade accountLogicFacade;
-    public StorageFacade storageFacade;
+    public AccountLogicFacade accountLogicFacade = new AccountLogicFacadeImpl(new AccountEntityFacadeDB());
+    public StorageFacade storageFacade = new StorageFacadeDB();
     
     @After
     public void tearDown() {
         storageFacade.emptyStorage();
     }
     
-    
-    
     @Test
     public void testCreate() {
+        accountLogicFacade.create("Tommy Lindman", "CHECK", "Swedbank");
     }
 }
